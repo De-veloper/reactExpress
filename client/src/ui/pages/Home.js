3f6d112cd53c58911e3b222c713b21afb6fd834b
Kevin Chiu <kchiu@paciolan.com>
2018-12-05 08:32:42 -0800

    updated home page

--- client/src/ui/pages/Home.js
+++ client/src/ui/pages/Home.js
@@ -9,13 +9,13 @@ class Home extends Component{
                 <Header titleTxt="To Do List"/>
                 <p>1. Write/Delete/Edit Json file</p>
                 <p>2. List all pixel for each clients</p>
-                <p>3. Make submit form to edit Json file (https://www.npmjs.com/package/react-form)</p>
+                <p>3. Make submit form to edit Json file (click <a href="https://www.npmjs.com/package/react-form" target="_blank">here</a>)</p>
                 <p>4. List all my clients</p>
                 <p>5. Redux for all nav </p>
                 <p>6. Redux Saga</p>
                 <p>7. Pixel CMS</p>
                 <p>8. My races</p>
-                <p>9. Added express server (https://www.codementor.io/kakarganpat/how-to-setup-react-and-node-js-in-a-project-koxwqbssl)</p>
+                <p>9. Added express server (<a href="https://www.codementor.io/kakarganpat/how-to-setup-react-and-node-js-in-a-project-koxwqbssl" target="_blank">Setup react with express</a>)</p>
             </div>
             
         )
