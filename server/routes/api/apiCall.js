f6500a3949bb60f742fd92d3bdf3185e30e1849d
Kevin Chiu <kchiu@paciolan.com>
2019-01-25 11:24:27 -0800

    worked on a feature to search certain string in .js file

--- server/routes/api/apiCall.js
+++ server/routes/api/apiCall.js
@@ -7,8 +7,72 @@ var pixelFile = './public/json/pixel.json';//data for exercise
 
 /* API main page */
 router.get('/', function(req, res, next) {
+    //count certain string 
+    fs.readFile('./public/json/test.js', 'utf-8', function (err, data) {
+        if (err) throw err;
+        var splitByWords = function(text) {
+            // split string by spaces (including spaces, tabs, and newlines)
+            var wordsArray = text.split(/\s+/);
+            return wordsArray;
+          }
+          
+          
+          var createWordMap = function(wordsArray) {
+          
+            // create map for word counts
+            var wordsMap = {};
+            /*
+              wordsMap = {
+                'Oh': 2,
+                'Feelin': 1,
+                ...
+              }
+            */
+            wordsArray.forEach(function (key) {
+              if (wordsMap.hasOwnProperty(key)) {
+                wordsMap[key]++;
+              } else {
+                wordsMap[key] = 1;
+              }
+            });
+          
+            return wordsMap;
+          
+          }
+          
+          
+          var sortByCount = function(wordsMap) {
+          
+            // sort by count in descending order
+            var finalWordsArray = [];
+            finalWordsArray = Object.keys(wordsMap).map(function(key) {
+              return {
+                name: key,
+                total: wordsMap[key]
+              };
+            });
+          
+            finalWordsArray.sort(function(a, b) {
+              return b.total - a.total;
+            });
+          
+            return finalWordsArray;
+          
+          }
+          console.log(data)
+        var wordsArray = splitByWords(data);
+        var wordsMap = createWordMap(wordsArray);
+        var finalWordsArray = sortByCount(wordsMap);
+
+        console.log(finalWordsArray);
+        console.log('The word "' + finalWordsArray[0].name + '" appears the most in the file ' +
+            finalWordsArray[0].total + ' times');
+    });
+    //end TODO
     res.render('api', { title: 'All API is here' });
 });
+
+
 
 
 /* Submit */
